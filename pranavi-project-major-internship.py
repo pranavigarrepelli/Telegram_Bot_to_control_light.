0705f5fbe43551a05f67feb9378d435a73250b8d
pranavigarrepelli <70741835+pranavigarrepelli@users.noreply.github.com>
2020-09-14 13:29:48 +0530

    Update pranavi-project-major-internship.py

--- pranavi-project-major-internship.py
+++ pranavi-project-major-internship.py
@@ -35,8 +35,8 @@ def input_message(bot,update):
       value=Data(value=0)
       value_send=aio.create_data('lightbot',value)
       
-Token=os.getenv('1049056873:AAG58ewf5tTJ3w7fQrRDXlAcKO5qppHOpaY')   
-u = Updater(Token)
+
+u = Updater(1049056873:AAG58ewf5tTJ3w7fQrRDXlAcKO5qppHOpaY)
 dp = u.dispatcher
 dp.add_handler(CommandHandler('turnon',turnon))
 dp.add_handler(CommandHandler('turnoff',turnoff))
